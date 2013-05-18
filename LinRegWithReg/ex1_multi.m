2742a7fe2e12b885824ec33f233d4c0aaf0d2971
Dev Mehta <dpmehta02@gmail.com>
2013-05-18 10:37:09 -0700

    normalized features

--- LinRegWithReg/ex1_multi.m
+++ LinRegWithReg/ex1_multi.m
@@ -1,30 +1,5 @@
-%% Machine Learning Online Class
-%  Exercise 1: Linear regression with multiple variables
-%
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
-%
-%  You will need to complete the following functions in this 
-%  exericse:
-%
-%     warmUpExercise.m
-%     plotData.m
-%     gradientDescent.m
-%     computeCost.m
-%     gradientDescentMulti.m
-%     computeCostMulti.m
-%     featureNormalize.m
-%     normalEqn.m
-%
-%  For this part of the exercise, you will need to change some
-%  parts of the code below for various experiments (e.g., changing
-%  learning rates).
-%
-
-%% Initialization
+%% Linear regression with multiple variables
+%% @dpmehta02 - adapted from Coursera's Machine Learning Course
 
 %% ================ Part 1: Feature Normalization ================
 
@@ -33,14 +8,14 @@ clear ; close all; clc
 
 fprintf('Loading data ...\n');
 
-%% Load Data
+%% Load Train Data [UPDATE THIS SECTION FOR EACH PROJECT] - Comma Separated, no Headers
 data = load('ex1data2.txt');
-X = data(:, 1:2);
-y = data(:, 3);
+X = data(:, 1:2); % Input variables
+y = data(:, 3); % Outcomes
 m = length(y);
 
-% Print out some data points
-fprintf('First 10 examples from the dataset: \n');
+% Print out 10 data points
+fprintf('First 10 examples: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
 fprintf('Program paused. Press enter to continue.\n');
